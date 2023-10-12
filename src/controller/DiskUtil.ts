c40849d0f355daeb1cf85d93536b740c5bf49ef9
Joshua Chew <joshchew@students.cs.ubc.ca>
2023-10-12 15:06:11 -0700

    fix path construction

--- src/controller/DiskUtil.ts
+++ src/controller/DiskUtil.ts
@@ -1,5 +1,5 @@
 import fs from "fs-extra";
 
 export function doesDatasetIDExist(id: string): boolean {
-	return fs.existsSync("./data" + id + ".json");
+	return fs.existsSync("./data/" + id + ".json");
 }
