114fb8f212629eaa0228d88f75c60adeb5099907
Aaron <afosoy.uni@gmail.com>
2023-10-13 06:31:15 -0700

    Add query checking for a given section - Validates if section should be added to result - Changed section year field to adhere to c0 specs (is a number)

--- src/queryScripts/ExecuteQuery.ts
+++ src/queryScripts/ExecuteQuery.ts
@@ -4,13 +4,17 @@ import {InsightResult} from "../controller/IInsightFacade";
 
 
 export function processQueryToAST(queryItem: any) {
+	// at top level key[0] WHERE has one key and value
 	let queryItemKey = Object.keys(queryItem)[0];
 	let itemChildren = queryItem[queryItemKey];
 
-	// if the current query item has a value that isn't a list we've reached our base case (some field)
-	// else we iterate through list to make a new node for each child
+	/* if the current query item has a value that isn't a list we've reached our base case (comparison with key : value)
+	   else we iterate through list to make a new node for each child */
 	if(!Array.isArray(itemChildren)) {
-		return new QueryASTNode(queryItemKey, itemChildren);
+		// make final node with key:value, add to list of MCOMPARISON/SCOMPARISON node
+		let leafItemKey = Object.keys(itemChildren)[0];
+		let leaf = new QueryASTNode(leafItemKey, itemChildren[leafItemKey]);
+		return new QueryASTNode(queryItemKey, [leaf]);
 	} else {
 		let currRoot = new QueryASTNode(queryItemKey, []);
 		for(let childItem of itemChildren) {
@@ -20,11 +24,41 @@ export function processQueryToAST(queryItem: any) {
 	}
 }
 
-// TODO: check if section should be included in result given query
-export function passesQuery(currSection: SectionPruned, query: QueryASTNode) {
+export function passesQuery(currSection: SectionPruned, query: QueryASTNode): boolean {
 	// if section doesn't pass any of the query execution return false; will only return true if query works
+	let includeSection = true;
+	let queryNodeKey = query.key;
+	let queryChildren = query.children as QueryASTNode[];
 
-	return true;
+	switch (queryNodeKey) {
+		case "AND":
+			for(const child of query.children as QueryASTNode[]) {
+				includeSection = includeSection && passesQuery(currSection, child);
+			}
+			return includeSection;
+		case "OR":
+			includeSection = false;
+			for(const child of queryChildren) {
+				includeSection = includeSection || passesQuery(currSection, child);
+			}
+			return includeSection;
+		case "LT":
+			return passesMComparator(currSection, queryChildren[0], "LT");
+		case "GT":
+			return passesMComparator(currSection, queryChildren[0], "GT");
+		case "EQ":
+			return passesMComparator(currSection, queryChildren[0], "EQ");
+		case "NOT": {
+
+			return !passesQuery(currSection, queryChildren[0]);
+		}
+		case "IS": {
+			return matchesSField(currSection, queryChildren[0]);
+
+		}
+		default:
+			return includeSection;
+	}
 }
 
 export function transformColumns(rawResult: SectionPruned[], columns: string[]) {
@@ -52,4 +86,48 @@ export function orderRows(result: InsightResult[], order: string) {
 		// keep order as is
 		return 0;
 	});
+}
+
+
+// HELPER FUNCTIONS
+
+function passesMComparator(section: SectionPruned, mComparison: QueryASTNode, mComparator: string) {
+	let fieldName = mComparison.key.split("_")[1];
+	let mValue: number = mComparison.children as number;
+	let sectionField: number = section.getField(fieldName) as number;
+	switch(mComparator) {
+		case "LT":
+			return sectionField < mValue;
+		case "GT":
+			return sectionField > mValue;
+		case "EQ":
+			return sectionField === mValue;
+		default:
+			return true;
+	}
+}
+
+function matchesSField(section: SectionPruned, sComparison: QueryASTNode) {
+	let fieldName = sComparison.key.split("_")[1];
+	let sValue: string = sComparison.children as string;
+	let field: string = section.getField(fieldName) as string;
+
+	if(sValue === "*" || sValue === "**") {
+		return true;
+	}
+
+	if(fieldName === "" && sValue !== "") {
+		return false;
+	}
+
+	// sValue isn't empty when removing asterisk
+	if (sValue.startsWith("*") && sValue.endsWith("*")) {
+		return field.includes(sValue.slice(1, -1));
+	} else if (sValue.startsWith("*")) {
+		return field.startsWith(sValue.slice(0, -1));
+	} else if (sValue.endsWith("*")) {
+		return field.endsWith(sValue.slice(1));
+	} else {
+		return sValue === field;
+	}
 }
