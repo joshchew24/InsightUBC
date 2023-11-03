b0d86d68479a30cb8e8d34146f2bf20c7ef390a5
Joshua Chew <joshchew@students.cs.ubc.ca>
2023-11-02 20:40:42 -0700

    update options validator to accomodate for new sort capabilities

--- src/queryScripts/ValidateOptions.ts
+++ src/queryScripts/ValidateOptions.ts
@@ -9,6 +9,7 @@ import {validateQueryKey} from "./ValidateQuery";
  	],
  	"ORDER": "XXX_XXX"
  }
+ Returns an array of the keys in COLUMNS
 */
 export function validateOptions(query: object) {
 	// TODO: redundant check, suppresses warnings when directly indexing query by "OPTIONS"
@@ -19,26 +20,64 @@ export function validateOptions(query: object) {
 	if (!("COLUMNS" in options)) {
 		throw new InsightError("OPTIONS missing COLUMNS");
 	}
-	let colKeys: string[] = options["COLUMNS"] as string[];
+	let colKeys = options["COLUMNS"] as string[];
+	validateColumns(colKeys);
+
+	// numKeys is number of keys in Options object.
+	let numKeys = Object.keys(options).length;
+	// 1 key means we should only have a COLUMN object
+	if (numKeys === 1) {
+		return;
+	}
+	// 2 keys means we should only have a COLUMN and ORDER object
+	if ((numKeys === 2 && !("ORDER" in options)) || numKeys > 2) {
+		throw new InsightError("Invalid keys in OPTIONS");
+	}
+	validateOrder(options["ORDER"], colKeys);
+}
+
+function validateColumns(colKeys: string[]) {
 	if (!Array.isArray(colKeys)) {
 		throw new InsightError("COLUMNS must be a non-empty array");
 	}
 	for (let colKey of colKeys) {
-		validateQueryKey("COLUMNS", colKey);
+		// intelliJ wrongly thinks this check is unnecessary
+		if (typeof colKey !== "string") {
+			throw new InsightError("Invalid query string");
+		}
+		if (colKey.includes("_")) {
+			validateQueryKey("COLUMNS", colKey);
+		}
 	}
-	// numKeys is number of keys in Options object.
-	// 1 key means we should only have a COLUMN object
-	// 2 keys means we should only have a COLUMN and ORDER object
-	let numKeys = Object.keys(options).length;
-	if (numKeys === 1) {
-		return;
-	}
-	if ((numKeys === 2 && !("ORDER" in options)) || numKeys > 2) {
-		throw new InsightError("Invalid keys in OPTIONS");
-	}
-	let orderKey: string = options["ORDER"] as string;
-	validateQueryKey("ORDER", orderKey);
-	if (!colKeys.includes(orderKey)) {
-		throw new InsightError("ORDER key must be in COLUMNS");
+}
+
+// {[index: string]: string | string[]} | string
+function validateOrder(order: unknown, colKeys: string[]) {
+	if (typeof order === "string") {
+		validateQueryKey("ORDER", order);
+		if (!colKeys.includes(order)) {
+			throw new InsightError("ORDER key must be in COLUMNS");
+		}
+	} else if (typeof order === "object" && order !== null) {
+		if (!("dir" in order)) {
+			throw new InsightError("ORDER missing 'dir' key");
+		}
+		let direction = order["dir"];
+		if (typeof direction !== "string" || !(direction === "UP" || direction === "DOWN")) {
+			throw new InsightError("Invalid ORDER direction");
+		}
+		if (!("keys" in order)) {
+			throw new InsightError("ORDER missing 'keys' key");
+		}
+		let orderKeys = order["keys"];
+		if (!Array.isArray(orderKeys) || orderKeys.length === 0) {
+			throw new InsightError("ORDER keys must be a non-empty array");
+		}
+		for (let orderKey of orderKeys) {
+			if (!colKeys.includes(orderKey)) {
+				throw new InsightError("All ORDER keys must be in COLUMNS");
+			}
+			// we will validate all colKeys, so if all orderKeys are in colKeys, they will get validated eventually
+		}
 	}
 }
