85d9bc405b20bf25d0c9853f5403df374afeee63
Aaron <afosoy.uni@gmail.com>
2023-11-03 02:24:08 -0700

    Setting up performQuery to take into account Rooms and Aggregation/New Order

--- src/models/IRoom.ts
+++ src/models/IRoom.ts
@@ -1,28 +1,3 @@
-// export interface Room {
-// 	fullname: string;    // Full building name.
-// 	shortname: string;   // Short building name.
-// 	number: string;      // The room number. Not always a number so represented as a string.
-// 	name: string;        // The room id. Should be rooms_shortname + "_" + rooms_number.
-// 	address: string;     // The building address.
-// 	lat: number;         // The latitude of the building.
-// 	lon: number;         // The longitude of the building.
-// 	seats: number;       // The number of seats in the room.
-// 	type: string;        // The room type.
-// 	furniture: string;   // The room furniture.
-// 	href: string;        // The link to the full details online.
-// }
-
-// // HTML DOM nodes from parse5
-export interface DomNode {
-	nodeName: string;
-	tagName?: string;
-	value?: string;
-	attrs?: Array<{[key: string]: any}>;
-	namespaceURI: string;
-	childNodes?: DomNode[];
-	parentNode?: DomNode;
-}
-
 export class Room {
 	public fullname: string;    // Full building name.
 	public shortname: string;   // Short building name.
@@ -36,29 +11,18 @@ export class Room {
 	public furniture: string;   // The room furniture.
 	public href: string;        // The link to the full details online.
 
-	constructor(
-		fullname: string,
-		shortname: string,
-		number: string,
-		address: string,
-		lat: number,
-		lon: number,
-		seats: number,
-		type: string,
-		furniture: string,
-		href: string
-	) {
-		this.fullname = fullname;
-		this.shortname = shortname;
-		this.number = number;
-		this.name = `${shortname}_${number}`;
-		this.address = address;
-		this.lat = lat;
-		this.lon = lon;
-		this.seats = seats;
-		this.type = type;
-		this.furniture = furniture;
-		this.href = href;
+	constructor(room: any) {
+		this.fullname = room.fullname;
+		this.shortname = room.shortname;
+		this.number = room.number;
+		this.name = room.name;
+		this.address = room.address;
+		this.lat = room.lat;
+		this.lon = room.lon;
+		this.seats = room.seats;
+		this.type = room.type;
+		this.furniture = room.furniture;
+		this.href = room.href;
 	}
 
 	public getField?(fieldName: string) {
@@ -101,7 +65,17 @@ export class Room {
 			default:
 				fieldValue = "";
 		}
-
 		return fieldValue;
 	}
+}
+
+// HTML DOM nodes from parse5
+export interface DomNode {
+	nodeName: string;
+	tagName?: string;
+	value?: string;
+	attrs?: Array<{[key: string]: any}>;
+	namespaceURI: string;
+	childNodes?: DomNode[];
+	parentNode?: DomNode;
 }
