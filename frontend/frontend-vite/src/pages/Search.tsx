fa234ad3f02b5b4e38f39e36a52580d8c2e8251c
joonjang <w.joon.jang@gmail.com>
2023-11-20 16:08:50 -0800

    ui works with mock data in terms of input

--- frontend/frontend-vite/src/pages/Search.tsx
+++ frontend/frontend-vite/src/pages/Search.tsx
@@ -1,6 +1,6 @@
 import {NavigationBar} from "../component/NavigationBar";
 import {
-	Autocomplete, Button,
+	Autocomplete, Button, Divider,
 	FormControl,
 	FormControlLabel,
 	FormLabel, Grid,
@@ -10,45 +10,97 @@ import {
 	TextField
 } from "@mui/material";
 import {useEffect, useState} from "react";
+import professorData from "../mockJson/professor.json";
+import departmentData from "../mockJson/department.json";
+
+interface Section {
+	sections_uuid: string;
+	sections_id: string;
+	sections_title: string;
+	sections_instructor: string;
+	sections_dept: string;
+	sections_year: number;
+	sections_avg: number;
+	sections_pass: number;
+	sections_fail: number;
+	sections_audit: number;
+}
+
+interface AutoComplete {
+	label: string;
+	value: Section[]
+}
+
+const groupByInstructor = (data: Section[]): AutoComplete[] => {
+	const result: Record<string, Section[]> = {};
+
+	data.forEach((section) => {
+		// Split the instructor names by comma
+		const instructors = section.sections_instructor.split(';');
+
+		instructors.forEach((instructor) => {
+			if (!result[instructor]) {
+				result[instructor] = [];
+			}
+			result[instructor].push(section);
+		});
+	});
+
+	return Object.keys(result).sort().map((instructor) => ({
+		label: instructor,
+		value: result[instructor]
+	}));
+};
+
+const groupByDepartment = (data: Section[]): AutoComplete[] => {
+	const result: Record<string, Section[]> = {};
+
+	data.forEach((section) => {
+		const department = section.sections_dept;
+
+		if (!result[department]) {
+			result[department] = [];
+		}
+		result[department].push(section);
+	});
+
+	return Object.keys(result).sort().map((dept) => ({
+		label: dept,
+		value: result[dept]
+	}));
+};
 
 
-const exampleProfessors = [
-	{
-		label: "Dr. John Doe",
-		name: "Dr. John Doe",
-	},
-	{
-		label: "Dr. Bill Doe",
-		value: "Dr. Bill Doe"
-	},
-	{
-		label: "Dr. Alex Smith",
-		value: "Dr. Alex Smith"
-	}
-]
+const groupedProfessorData = groupByInstructor(professorData);
+const groupedDepartmentData = groupByDepartment(departmentData);
 
-const exampleDepartment = [
-	{
-		label: "CPSC",
-		value: "Computer Science",
-	},
-	{
-		label: "FRST",
-		value: "Forestry",
-	},
-	{
-		label: "WOOD",
-		value: "Wood Products Processing",
-	}
-]
 
 export function Search() {
 
 	const [searchType, setSearchType] = useState("");
+	const [deptYear, setDeptYear] = useState<string | null>(null);
+	const [userInput, setUserInput] = useState<string | null>(null);
+	const [searchedData, setSearchedData] = useState<Section[] | null>(null);
 
 	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setSearchType((event.target as HTMLInputElement).value);
 	};
+
+	const handleSearch = () => {
+		if (searchType === "professor") {
+			groupedProfessorData.forEach((professor) => {
+				if (professor.label === userInput) {
+					setSearchedData(professor.value);
+				}
+			});
+		} else if (searchType === "department") {
+			groupedDepartmentData.forEach((department) => {
+				if (department.label === userInput) {
+					setSearchedData(department.value);
+				}
+			});
+		}
+	}
 
 	useEffect(() => {
 		document.body.style.background = `url(fire.jpg)`
@@ -84,28 +136,101 @@ export function Search() {
 					</Grid>
 
 					<Grid item container>
+
 			<Autocomplete
 				disablePortal
 				id="combo-box-demo"
+				onInputChange={(_event, value: string | null) => {
+						setUserInput(value);
+						setSearchedData(null)
+				}}
 				options={
-					searchType === "professor" ? exampleProfessors :
-					searchType === "department" ? exampleDepartment : []
+					searchType === "professor" ? groupedProfessorData :
+					searchType === "department" ? groupedDepartmentData : []
 				}
 				sx={{ width: 300 }}
 				renderInput={(params) =>
 					<TextField {...params} label={searchType == "" ? "Choose Search Type" : searchType } />}
 				disabled={searchType === ""}
 			/>
+						{searchType === "department" && <TextField
+							id="department-year"
+							label="Year"
+							value={deptYear}
+							type="number"
+							onChange={(event) => {
+								if(event.target.value === "") {
+									setDeptYear(null);
+									return;
+								}
+								setDeptYear(event.target.value);
+							}}
+
+						/>
+
+								}
 
 				<Button
 					variant="contained"
-					disabled={searchType === ""}
+					disabled={searchType === "" || userInput === null}
+					onClick={handleSearch}
 					sx={{
 					padding: "15px"
 				}} > Search </Button>
 					</Grid>
 				</Grid>
 			</Paper>
+
+			{searchedData && <Paper sx={{
+				padding: 1,
+				marginTop: 1,
+				maxHeight: 500,
+				overflow: 'auto'
+			}}
+			>
+				{searchedData.map((section) => (
+					<>
+					<Grid item key={section.sections_uuid} sx={{
+						padding:2,
+					}}
+					>
+						<Grid item>
+							<u>Title</u>: {section.sections_title}
+						</Grid>
+						<Grid item>
+							<u>Audit</u>: {section.sections_audit}
+						</Grid>
+						<Grid item>
+						<u>Average</u>: {section.sections_avg}
+						</Grid>
+						<Grid item>
+							<u>Department</u>: {section.sections_dept}
+						</Grid>
+						<Grid item>
+							<u>Fail</u>: {section.sections_fail}
+						</Grid>
+						<Grid item>
+							<u>ID</u>: {section.sections_id}
+						</Grid>
+						<Grid item>
+							<u>Instructor(s)</u>: {section.sections_instructor.split(';').map((instructor) => (
+							<>
+								{instructor}
+								<br/>
+							</>
+						))}
+						</Grid>
+						<Grid item>
+						Pass: {section.sections_pass}
+						</Grid>
+						<Grid item>
+							<u>Year</u>: {section.sections_year}
+						</Grid>
+					</Grid>
+					<Divider />
+					</>
+				))}
+			</Paper>}
 		</div>
 	)
 }
