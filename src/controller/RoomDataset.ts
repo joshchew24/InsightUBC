8cbbc82fbb133082881f145926fecc7a906ef602
Joshua Chew <joshchew@students.cs.ubc.ca>
2023-11-23 01:25:48 -0800

    extract helper method and promisify finding building table

--- src/controller/RoomDataset.ts
+++ src/controller/RoomDataset.ts
@@ -5,6 +5,7 @@ import {defaultTreeAdapter, parse} from "parse5";
 import * as parse5 from "parse5";
 import {ChildNode, Document, Element} from "parse5/dist/tree-adapters/default";
 import {Attribute} from "parse5/dist/common/token";
+import {makeAsync} from "./AsyncUtil";
 
 export class RoomDataset extends InsightDatasetClass {
 	private readonly ROOMS_DIR = "campus/discover/buildings-and-classrooms";
@@ -29,55 +30,54 @@ export class RoomDataset extends InsightDatasetClass {
 			})
 			.then((index: string) => {
 				let document: Document = parse(index);
-				if (!document || !document.childNodes) {
+				if (!document || defaultTreeAdapter.getChildNodes(document).length === 0) {
 					throw new InsightError("index is empty");
 				}
-				let buildingTableSearchStack: ChildNode[] = defaultTreeAdapter.getChildNodes(document);
-				// TODO: can we move these declarations inside the loop for better readability?
-				let currNode: ChildNode;
-				let currElem: Element;
-				let attributes: Attribute[];
-				let buildingTable: Element;
-				searchLoop: while (buildingTableSearchStack.length > 0) {
-					currNode = buildingTableSearchStack.pop() as ChildNode;
-					// if (!currNode) {
-					// 	// shouldn't happen because of loop invariant but needed to suppress warnings
-					// 	continue;
-					// }
-					if ("childNodes" in currNode) {
-						buildingTableSearchStack.push(...defaultTreeAdapter.getChildNodes(currNode));
-					}
-					if (!defaultTreeAdapter.isElementNode(currNode)
-						|| defaultTreeAdapter.getTagName(currNode) !== parse5.html.TAG_NAMES.TABLE) {
-						continue;
-					}
-					currElem = currNode as Element;
-					attributes = defaultTreeAdapter.getAttrList(currElem);
-					if (!attributes) {
-						continue;
-					}
-					for (let attribute of attributes) {
-						if (attribute.name === "class" && attribute.value.includes("views-table")) {
-							buildingTable = currElem;
-							break searchLoop;
-						}
-					}
-					// if (!("class" in currNode.attrs)) {
-					// 	continue;
-					// }
-					// let classes = currNode.attrs["class"];
-					// if (classes.includes("views-field")) {
-					// 	// do stuff
-					// }
-						// && "class" in currNode.attrs
-						// && currNode.attrs["class"].includes("views-field")) {
-						// do stuff
-				}
+				return makeAsync(this.findBuildingTable,"No valid building table", [document]);
+			}).then((buildingTable) => {
+				// if (buildingTable == null) {
+				// 	throw new InsightError("no valid building table");
+				// }
 				return Promise.resolve(["asdf"]);
-				// let buildingTable = this.findBuildingTable(document);
+			})
+			.catch((err) => {
+				throw err;
 			});
 		// WIP
 		// return Promise.resolve(["hi"]);
+	}
+
+
+	private findBuildingTable(document: Document): Element | null {
+		let buildingTableSearchStack: ChildNode[] = defaultTreeAdapter.getChildNodes(document);
+		// TODO: can we move these declarations inside the loop for better readability?
+		let currNode: ChildNode;
+		let currElem: Element;
+		let attributes: Attribute[];
+		let buildingTable: Element | null = null;
+		searchLoop: while (buildingTableSearchStack && buildingTableSearchStack.length > 0) {
+			currNode = buildingTableSearchStack.pop() as ChildNode;
+			if ("childNodes" in currNode) {
+				buildingTableSearchStack.push(...defaultTreeAdapter.getChildNodes(currNode));
+			}
+			if (!defaultTreeAdapter.isElementNode(currNode)
+				|| defaultTreeAdapter.getTagName(currNode) !== parse5.html.TAG_NAMES.TABLE) {
+				continue;
+			}
+			currElem = currNode as Element;
+			attributes = defaultTreeAdapter.getAttrList(currElem);
+			if (!attributes) {
+				continue;
+			}
+			// TODO: stricter check that this is the correct table (find a TD with a views-field class)
+			for (let attribute of attributes) {
+				if (attribute.name === "class" && attribute.value.includes("views-table")) {
+					buildingTable = currElem;
+					break searchLoop;
+				}
+			}
+		}
+		return buildingTable;
 	}
 }
 
